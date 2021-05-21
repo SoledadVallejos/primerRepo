96f763ba4f77eeeebc59d4ee063add4d6a379d3a
Soledad Vallejos <soledad_vallejos@hotmail.com>
2021-05-21 00:56:37 -0300

    mi primer commit

---
+++ gatos.js
@@ -0,0 +1,16 @@
+let gatos =  [ {
+    nombre: "Pritty",
+    edad: 4,
+    color: "Amarillo",
+    sexo: "macho",
+    adoptado: true
+},
+{
+    nombre: "Billy",
+    edad: 5,
+    color: "Blaco",
+    sexo: "macho",
+    adoptado: true 
+}] 
+
+console.log(gatos[0].edad);
